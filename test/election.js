f25ec846e6c5107f35ad2a0b7ccb82ea66c14702
ckwong <chunkang.wong94@gmail.com>
2023-09-17 01:15:39 +0800

    Converted to async await

--- test/election.js
+++ test/election.js
@@ -22,4 +22,16 @@ contract("Election", (accounts) => {
     assert.equal(candidate2[1], "Candidate 2", "contains the correct name");
     assert.equal(candidate2[2], 0, "contains the correct votes count");
   });
+
+  it("allows a voter to cast a vote", async () => {
+    const candidateId = 1;
+    await electionInstance.vote(candidateId, {
+      from: accounts[0],
+    });
+    const voted = await electionInstance.voters(accounts[0]);
+    assert(voted, "the voter was marked as voted");
+    const candidate = await electionInstance.candidates(candidateId);
+    const voteCount = candidate[2];
+    assert.equal(voteCount, 1, "increments the candidate's vote count");
+  });
 });
