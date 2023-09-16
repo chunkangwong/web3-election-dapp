0ba411d36a2a22603ce4aa6d388a544bd26b1e9a
ckwong <chunkang.wong94@gmail.com>
2023-09-17 00:41:48 +0800

    Updated constructor syntax

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.16;
 contract Election {
     string public candidate;
 
-    function Election () public {
+    constructor () public {
         candidate = "Candidate 1";
     }
 }
