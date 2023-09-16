7ae4292cc23df84e761382ce11e7df1654130ff6
ckwong <chunkang.wong94@gmail.com>
2023-09-17 01:13:35 +0800

    Implemented vote function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,7 @@ contract Election {
     }
 
     mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
     uint public candidatesCount;
 
@@ -19,5 +20,12 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
     }
 }
