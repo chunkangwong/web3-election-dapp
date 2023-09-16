32ec6e4d1a552f642ac7a8e23829da78333c14b7
ckwong <chunkang.wong94@gmail.com>
2023-09-17 01:01:05 +0800

    Converted to async await

--- test/election.js
+++ test/election.js
@@ -3,32 +3,23 @@ const Election = artifacts.require("./Election.sol");
 contract("Election", (accounts) => {
   let electionInstance;
 
-  it("initializes with two candidates", function () {
-    return Election.deployed()
-      .then(function (instance) {
-        return instance.candidatesCount();
-      })
-      .then(function (count) {
-        assert.equal(count, 2);
-      });
+  before(async () => {
+    electionInstance = await Election.deployed();
   });
 
-  it("it initializes the candidates with the correct values", function () {
-    return Election.deployed()
-      .then(function (instance) {
-        electionInstance = instance;
-        return electionInstance.candidates(1);
-      })
-      .then(function (candidate) {
-        assert.equal(candidate[0], 1, "contains the correct id");
-        assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-        assert.equal(candidate[2], 0, "contains the correct votes count");
-        return electionInstance.candidates(2);
-      })
-      .then(function (candidate) {
-        assert.equal(candidate[0], 2, "contains the correct id");
-        assert.equal(candidate[1], "Candidate 2", "contains the correct name");
-        assert.equal(candidate[2], 0, "contains the correct votes count");
-      });
+  it("initializes with two candidates", async () => {
+    const count = await electionInstance.candidatesCount();
+    assert.equal(count, 2);
+  });
+
+  it("it initializes the candidates with the correct values", async () => {
+    const candidate1 = await electionInstance.candidates(1);
+    assert.equal(candidate1[0], 1, "contains the correct id");
+    assert.equal(candidate1[1], "Candidate 1", "contains the correct name");
+    assert.equal(candidate1[2], 0, "contains the correct votes count");
+    const candidate2 = await electionInstance.candidates(2);
+    assert.equal(candidate2[0], 2, "contains the correct id");
+    assert.equal(candidate2[1], "Candidate 2", "contains the correct name");
+    assert.equal(candidate2[2], 0, "contains the correct votes count");
   });
 });
